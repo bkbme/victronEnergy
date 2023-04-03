550785141637c71353c0b49d3e680ab7f4948a95
christian1980nrw <christian1980nrw@gmail.com>
2023-04-03 22:40:55 +0200

    Update TWE_Eastron_SDM72D.py

--- src/energyMeter/dbus-modbus-client/TWE_Eastron_SDM72D.py
+++ src/energyMeter/dbus-modbus-client/TWE_Eastron_SDM72D.py
@@ -12,6 +12,7 @@
 # It should also work with the cheaper https://amzn.to/3KkEROl (without MID certification, currently 54 EUR).
 # RS485 USB adapter: https://amzn.to/3Jw5lL8 (the version with FT232RL chip, currently 18.50 EUR)
 # First set your meter to 19200 baud (see manual). The default meter password is 1000. Second place code in /opt/victronenergy/.
+# See installation notes at https://community.victronenergy.com/idea/114716/power-meter-lib-for-modbus-rtu-based-meters-from-a.html
 
 import logging
 import TWE_Eastron_device as device
